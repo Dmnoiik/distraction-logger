5845af87436700f303b1e3f7a145e1fcaf660c75
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2025-06-20 15:18:53 +0200

    Implemented logsPerDay

--- Data/LogsData/DistractionLogRepository.cs
+++ Data/LogsData/DistractionLogRepository.cs
@@ -45,7 +45,6 @@ namespace Distraction_Logger_PWA.Data.LogsData
         {
             DateTime endDate = DateTime.Now.Date.AddDays(1);
             DateTime startDate = endDate.Date.AddDays(-days);
-
             var query = await GetQueryAsync();
             var logs = await query.Where(log => log.Date >= startDate && log.Date <= endDate).ToListAsync();
             return logs;
